6a2ba7d785aa2a40c8cb5fdf22d1141e6544b47e
qiuweikangdev <642827353@qq.com>
2024-02-16 02:19:29 +0800

    chore: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,40 @@ module.exports = {
     '@vue/eslint-config-prettier'
   ],
   rules: {
+    'no-var': 'error',
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'prefer-const': 'off',
+    'no-use-before-define': 'off',
+
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-empty-function': 'error',
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
+    'vue/script-setup-uses-vars': 'error',
+    'vue/v-slot-style': 'error',
+    'vue/no-mutating-props': 'error',
+    'vue/custom-event-name-casing': 'error',
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/attribute-hyphenation': 'error',
+    'vue/attributes-order': 'off',
+    'vue/no-v-html': 'off',
     'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    'vue/no-setup-props-destructure': 'off',
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      {
+        registeredComponentsOnly: false,
+        ignores: []
+      }
+    ]
   }
 }
