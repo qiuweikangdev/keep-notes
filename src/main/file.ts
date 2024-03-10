f00b04b69f289e442fb910bbce8e24b97d0f9f5a
qiuweikangdev <642827353@qq.com>
2024-03-10 22:10:59 +0800

    feat: optimize readDirectory

--- src/main/file.ts
+++ src/main/file.ts
@@ -3,43 +3,66 @@ import path from 'node:path'
 import { dialog, ipcMain } from 'electron/main'
 import { getBrowserWindow } from './utils'
 
+// 过滤目录
+const ignoreDir = ['node_modules']
+
 // 递归读取目录结构，生成目录树
 async function readDirectory(directoryPath) {
   try {
     const files = await fs.promises.readdir(directoryPath)
-    const tree: any[] = []
 
-    await Promise.all(
-      files.map(async (file) => {
-        const filePath = path.join(directoryPath, file)
-        const stats = await fs.promises.stat(filePath)
+    // 分别存放文件和目录
+    const directories: string[] = []
+    const markdownFiles: string[] = []
 
-        if (stats.isDirectory()) {
-          const subtree = await readDirectory(filePath)
-          tree.push({
-            fileName: file,
-            title: file,
-            name: file,
-            text: file,
-            label: file,
-            value: filePath,
-            key: filePath,
-            children: subtree,
-          })
-        }
-        else if (['.md', '.txt'].includes(path.extname(file))) {
-          tree.push({
-            fileName: file,
-            title: file,
-            name: file,
-            text: file,
-            label: file,
-            value: filePath,
-            key: filePath,
-          })
+    // 遍历目录下的文件和目录
+    for (const file of files) {
+      const filePath = path.join(directoryPath, file)
+      const stats = await fs.promises.stat(filePath)
+
+      // 过滤掉忽略的目录和隐藏文件
+      if (
+        stats.isDirectory()
+        && !ignoreDir.includes(file)
+        && !file.startsWith('.')
+      ) {
+        directories.push(file)
+      }
+      else if (['.md', '.txt'].includes(path.extname(file))) {
+        markdownFiles.push(file)
+      }
+    }
+
+    // 对文件和目录进行排序、对文件和目录按照字母顺序进行排序，忽略大小写
+    directories.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+    markdownFiles.sort((a, b) =>
+      a.toLowerCase().localeCompare(b.toLowerCase()),
+    )
+
+    // 递归读取子目录的目录树
+    const directoryTrees = await Promise.all(
+      directories.map(async (dir) => {
+        const subtree = await readDirectory(path.join(directoryPath, dir))
+        return {
+          fileName: dir,
+          title: dir,
+          value: path.join(directoryPath, dir),
+          key: path.join(directoryPath, dir),
+          children: subtree,
         }
       }),
     )
+
+    // 拼接文件和目录的目录树
+    const tree = [
+      ...directoryTrees,
+      ...markdownFiles.map(file => ({
+        fileName: file,
+        title: file,
+        value: path.join(directoryPath, file),
+        key: path.join(directoryPath, file),
+      })),
+    ]
 
     return tree
   }
