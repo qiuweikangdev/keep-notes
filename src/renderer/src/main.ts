11d34d3e1d7bafb1950a6090e779f42431779407
qiuweikangdev <642827353@qq.com>
2024-12-23 00:49:49 +0800

    style: panel style

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -3,6 +3,5 @@ import App from './App.vue'
 import router from './router/index'
 import pinia from './store/index'
 import './assets/css/style.less'
-import '@milkdown/theme-nord/style.css'
 
 createApp(App).use(router).use(pinia).mount('#app')
